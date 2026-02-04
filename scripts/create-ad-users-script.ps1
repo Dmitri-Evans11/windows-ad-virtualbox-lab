4e226f82e952b653822f184358365c93bc9c2cd7
Dmitri-Evans11 <devans021@outlook.com>
2026-02-04 10:38:34 -0800

    Update create-ad-users-script.ps1

--- scripts/create-ad-users-script.ps1
+++ scripts/create-ad-users-script.ps1
@@ -1,1 +1,19 @@
+Import-Module ActiveDirectory
 
+$users = Import-Csv ".\users.csv"
+
+foreach ($User in $users) {
+
+    $FullName = "$($User.FirstName) $($User.LastName)"
+
+    New-ADUser `
+        -Name $FullName `
+        -GivenName $User.FirstName `
+        -Surname $User.LastName `
+        -SamAccountName $User.Username `
+        -UserPrincipalName "$($User.Username)@d.evans.adlab" `
+        -Path "OU=user-accounts,DC=d,DC=evans,DC=adlab" `
+        -AccountPassword (ConvertTo-SecureString $User.Password -AsPlainText -Force) `
+        -Enabled $true `
+        -ChangePasswordAtLogon $true
+}
